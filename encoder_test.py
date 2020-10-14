b6c42bbe9ff938c7963dc5b3701d6948a5274bfd
Rob Swinbank <rob@m0dts.co.uk>
2020-10-14 13:24:17 +0100

    Create encoder_test.py
    Example test script

---
+++ encoder_test.py
@@ -0,0 +1,19 @@
+from encoder import encoder #include encoder.py
+
+
+enc = encoder("192.168.88.252","m0dts","xxxxx")    #setup new encoder instance
+#enc.restartUDP()   #UDP is normally enabled by the Pluto but can need re-starting
+enc.set_streaming_source()
+enc.streaming("SD")
+enc.streaming("OFF")
+enc.streaming("HD")
+
+enc.create_osd_image("M0DTS",64)
+enc.upload_osd_image(0,"image.jpg")     #MainStream
+enc.disable_osd_image(0)
+enc.enable_osd_image(0,15,15,64)
+
+#enc.create_osd_image("M0DTS",48)
+#enc.upload_osd_image(1,"image.jpg")    #SubStream
+#enc.disable_osd_image(1)
+#enc.enable_osd_image(1,15,15,64)
